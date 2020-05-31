d5b92d59a718c8914283719961bb0c1ce0887bbc
RioS101 <rio.s101101@gmail.com>
2020-05-31 14:05:01 +0400

    m1

---
+++ Experiment/ColorCell.swift
@@ -0,0 +1,13 @@
+//
+//  ColorCell.swift
+//  Experiment
+//
+//  Created by Riad on 5/31/20.
+//  Copyright © 2020 Projectum. All rights reserved.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+}
--- Experiment/ViewController.swift
+++ Experiment/ViewController.swift
@@ -2,19 +2,42 @@
 //  ViewController.swift
 //  Experiment
 //
-//  Created by Riad on 5/27/20.
+//  Created by Riad on 5/31/20.
 //  Copyright © 2020 Projectum. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    let totalColors: Int = 100
+    func colorForIndexPath(indexPath: NSIndexPath) -> UIColor {
+        if indexPath.row >= totalColors {
+            return UIColor.black    // return black if we get an unexpected row index
+        }
+        
+        var hueValue: CGFloat = CGFloat(indexPath.row) / CGFloat(totalColors)
+        return UIColor(hue: hueValue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
