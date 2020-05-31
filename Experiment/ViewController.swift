8851726d9853a5fa85d9881c4afb3bd3094ae2ae
RioS101 <rio.s101101@gmail.com>
2020-05-31 14:42:45 +0400

    basic collection view data rendering

--- Experiment/ViewController.swift
+++ Experiment/ViewController.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return totalColors
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath) as! ColorCell
+        let cellColor = colorForIndexPath(indexPath: indexPath)
+        cell.backgroundColor = cellColor
+        
+        if cellColor.cgColor.numberOfComponents == 4 {
+            let redComponent = cellColor.cgColor.components![0] * 255
+            let greenComponent = cellColor.cgColor.components![1] * 255
+            let blueComponent = cellColor.cgColor.components![2] * 255
+            cell.colorLabel.text = String(format: "%.0f, %.0f, %.0f", redComponent, greenComponent, blueComponent)
+        }
+        return cell
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
@@ -15,13 +35,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
 
     let totalColors: Int = 100
-    func colorForIndexPath(indexPath: NSIndexPath) -> UIColor {
+    func colorForIndexPath(indexPath: IndexPath) -> UIColor {
         if indexPath.row >= totalColors {
             return UIColor.black    // return black if we get an unexpected row index
         }
